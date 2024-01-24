8a6e2a4e2144d8484ebb634521746834c1f66736
Ozzeek <osama.m.alnagar@gmail.com>
2024-01-25 01:29:17 +0200

    feat: added message for win and loss

---
+++ src/Message.jsx
@@ -0,0 +1,14 @@
+export default function Message({ text }) {
+  return (
+    <div className=" flex aspect-square flex-col gap-8 rounded-2xl bg-red-500 p-8 shadow-xl">
+      <img
+        src={"./src/assets/" + text + ".jpeg"}
+        alt=""
+        className=" aspect-square rounded-lg object-cover"
+      />
+      <h1 className=" text-center text-3xl text-white">
+        {text === "lost" ? "خسرت" : "كسبت"}
+      </h1>
+    </div>
+  );
+}
