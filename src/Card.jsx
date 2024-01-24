5bd4c28492170f44149ec23f2045fb4a1cbdb817
Ozzeek <osama.m.alnagar@gmail.com>
2024-01-25 00:28:00 +0200

    feat: added images and modified logic

--- src/Card.jsx
+++ src/Card.jsx
@@ -1,12 +1,12 @@
 export default function Card({ url, onClick }) {
   return (
     <div
-      className=" inline-block flex h-40 w-40 items-center justify-center rounded-lg bg-amber-600"
+      className=" flex h-40 w-40 items-center justify-center rounded-lg bg-amber-600"
       onClick={onClick}
       id={url}
     >
       {/* ask chagGpt how to disable the child elements from being clicked */}
-      <div>{url}</div>
+      <img src={url} alt="" className=" h-32 w-32 object-cover" />
     </div>
   );
 }
--- src/Game.jsx
+++ src/Game.jsx
@@ -1,22 +1,48 @@
 import { useRef, useState } from "react";
 import Card from "./Card";
 
-const images = [1, 2, 3, 4, 5, 6, 7, 8];
+// const images = [1, 2, 3, 4, 5, 6, 7, 8];
+const images = [];
+
+for (let i = 1; i <= 8; i++) {
+  images.push({
+    id: i,
+    src: `./src/assets/images/${i}.jpeg`,
+  });
+}
+
+function randomize(array) {
+  let currentIndex = array.length - 1;
+
+  while (currentIndex > 0) {
+    const randomIndex = Math.floor(Math.random() * currentIndex);
+
+    [array[currentIndex], array[randomIndex]] = [
+      array[randomIndex],
+      array[currentIndex],
+    ];
+
+    currentIndex--;
+  }
+  return array;
+}
 
 export default function Game() {
   const [clickedImages, setClickedImages] = useState([]);
+  randomize(images);
   const score = useRef(0);
   const highScore = useRef(0);
+  console.log(clickedImages);
 
-  function clickEvent(e) {
-    if (clickedImages.includes(e.target.id)) {
+  function clickEvent(id) {
+    if (clickedImages.includes(id)) {
       setClickedImages([]);
       alert("your score is " + score.current);
       if (score.current > highScore.current) highScore.current = score.current;
       score.current = 0;
     } else {
       const newClickedImage = [...clickedImages];
-      newClickedImage.push(e.target.id);
+      newClickedImage.push(id);
       setClickedImages(newClickedImage);
       score.current++;
     }
@@ -27,7 +53,13 @@ export default function Game() {
       <p>{"High Score is " + highScore.current}</p>
       <div className="grid grid-cols-4 gap-4">
         {images.map((image) => {
-          return <Card key={image} url={image} onClick={clickEvent} />;
+          return (
+            <Card
+              key={image.id}
+              url={image.src}
+              onClick={() => clickEvent(image.id)}
+            />
+          );
         })}
       </div>
     </>
